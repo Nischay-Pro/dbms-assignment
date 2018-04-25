effe80434a818823517fab21f4b91e382a7b3376
Nischay Pro <NischayPro@gmail.com>
2018-04-26 01:57:53 +0530

    code: dashboard and login done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,10 @@
+var passport = require('passport');
 var connection = require('./mysql');
 var config = require('../config');
 var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require('bcrypt-nodejs');
 
-connection.query('USE' + config.database);
+connection.query('USE ' + config.database);
 
 module.exports = function (passport) {
 	passport.serializeUser(function (user, done) {
@@ -11,7 +12,7 @@ module.exports = function (passport) {
 	});
 
 	passport.deserializeUser(function (id, done) {
-		connection.query(`SELECT * FROM ${config.usersTable} WHERE id = ${id}`, function (err, rows) {
+		connection.query(`SELECT * FROM ${config.usersTable} WHERE id = '${id}'`, function (err, rows) {
 			done(err, rows[0]);
 		});
 	});
@@ -19,61 +20,54 @@ module.exports = function (passport) {
 	passport.use(
 		'local-signup',
 		new LocalStrategy({
-			// by default, local strategy uses username and password, we will override with email
 			usernameField: 'username',
 			passwordField: 'password',
-			passReqToCallback: true // allows us to pass back the entire request to the callback
-		},
-		function (req, username, password, done) {
-			// find a user whose email is the same as the forms email
-			// we are checking to see if the user trying to login already exists
-			connection.query('SELECT * FROM users WHERE username = ?', [username], function (err, rows) {
-				if (err)
-					return done(err);
-				if (rows.length) {
-					return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-				} else {
-					// if there is no user with that username
-					// create the user
-					var newUserMysql = {
-						username: username,
-						password: bcrypt.hashSync(password, null, null) // use the generateHash function in our user model
-					};
-
-					var insertQuery = `INSERT INTO users ( username, password ) values (${username},${password})`;
-
-					connection.query(insertQuery, function (err, rows) {
-						newUserMysql.id = rows.insertId;
-
-						return done(null, newUserMysql);
-					});
-				}
-			});
-		})
-	);
+			passReqToCallback: true
+			},
+			function (req, username, password, done) {
+				connection.query(`SELECT * FROM ${config.usersTable} WHERE username = '${username}'`, function (err, rows) {
+					if (err)
+						return done(err);
+					if (rows.length) {
+						// return done(null, false, req.flash('signupMessage', 'That username is already taken.')); 
+					} else {
+						var newUserMysql = {
+							username: username,
+							password: bcrypt.hashSync(password, null, null) // use the generateHash function in our user model
+						};
+						var insertQuery = `INSERT INTO ${config.usersTable} (username, password) values ('${username}', '${newUserMysql.password}')`;
+						connection.query(insertQuery, function (err, rows) {
+							newUserMysql.id = rows.insertId;
+							return done(null, newUserMysql);
+						});
+					}
+				});
+			}));
 	passport.use(
 		'local-login',
 		new LocalStrategy({
-			// by default, local strategy uses username and password, we will override with email
-			usernameField : 'username',
-			passwordField : 'password',
-			passReqToCallback : true // allows us to pass back the entire request to the callback
-		},
-		function(req, username, password, done) { // callback with email and password from our form
-			connection.query(`SELECT * FROM ${config.usersTable} WHERE username = ${username}`, function(err, rows){
-				if (err)
-					return done(err);
-				if (!rows.length) {
-					return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-				}
+				usernameField: 'username',
+				passwordField: 'password',
+				passReqToCallback: true
+			},
+			function (req, username, password, done) { // callback with email and password from our form
+				connection.query(`SELECT * FROM ${config.usersTable} WHERE username = '${username}'`, function (err, rows) {
+					if (err)
+						return done(err);
+					if (!rows.length) {
+						return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+					}
 
-				// if the user is found but the password is wrong
-				if (!bcrypt.compareSync(password, rows[0].password))
-					return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+					// if the user is found but the password is wrong
+					if (!bcrypt.compareSync(password, rows[0].password))
+						return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-				// all is well, return successful user
-				return done(null, rows[0]);
-			});
-		})
-	);
+					// all is well, return successful user
+					return done(null, rows[0]);
+				});
+			}));
+
+	connection.on('error', function (err) {
+		console.log(err.code);
+	});
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function (app, passport) {
+	app.get('/', function (req, res) {
+		res.render('index'); 
+	}); 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
-});
+	app.get('/login', function (req, res) {
+		res.render('login',  {
+			message:req.flash('loginMessage')
+		}); 
+	}); 
 
-module.exports = router;
+	app.post('/login', passport.authenticate('local-login',  {
+			successRedirect:'/dashboard/index', 
+			failureRedirect:'/login', 
+			failureFlash:true
+		}), 
+		function (req, res) {
+			if (req.body.remember) {
+				req.session.cookie.maxAge = 1000 * 60 * 3; 
+			}else {
+				req.session.cookie.expires = false; 
+			}
+			res.redirect('/'); 
+		}); 
+
+	app.get('/create', function (req, res) {
+		res.render('create',  {
+			message:req.flash('signupMessage')
+		}); 
+	}); 
+
+	app.post('/create', passport.authenticate('local-signup',  {
+		successRedirect:'/dashboard/index', 
+		failureRedirect:'/create', 
+		failureFlash:true
+	})); 
+
+	app.get('/logout', function (req, res) {
+		req.logout(); 
+		res.redirect('/'); 
+	}); 
+
+	app.get('/dashboard', isLoggedIn, (req, res) =>  {
+		res.render('dashboard/index'); 
+	});
+}; 
+
+function isLoggedIn(req, res, next) {
+
+	if (req.isAuthenticated())
+		return next(); 
+
+	res.redirect('/'); 
+}; 
