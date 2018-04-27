65060da77ff5a2c43aa78710019c8f6305aa5517
Nischay Pro <NischayPro@gmail.com>
2018-04-27 07:02:35 +0530

    code: donor ui

--- routes/dashboard/donors/index.js
+++ routes/dashboard/donors/index.js
@@ -7,8 +7,8 @@ module.exports = function (app, passport) {
 
     app.get('/dashboard/donors', isLoggedIn, (req, res) => {
         connection.query(`SELECT * FROM blood_donors WHERE deleted=0`, (err, rows) => {
-            if(err){
-                
+            if (err) {
+
             }
             res.render('dashboard/donors/index', {
                 username: req.user.username,
@@ -24,9 +24,40 @@ module.exports = function (app, passport) {
     });
 
     app.post('/dashboard/donors/create', isLoggedIn, (req, res) => {
-        res.render('dashboard/donors/create', {
-            username: req.user.username
-        });
+        try {
+            let elements = {}
+            elements.full_name = connection.escape(req.body.firstName) + ' ' + connection.escape(req.body.lastName);
+            elements.gender = connection.escape(req.body.gender);
+            elements.birthday = connection.escape(req.body.birthday);
+            elements.country = connection.escape(req.body.country);
+            elements.state = connection.escape(req.body.state);
+            elements.city = connection.escape(req.body.city);
+            elements.pincode = connection.escape(req.body.pincode);
+            elements.blood_group = connection.escape(req.body.bloodGroup);
+            elements.phone = connection.escape(req.body.phone);
+            elements.address = connection.escape(req.body.address1) + ' ' + connection.escape(req.body.address2);
+            connection.query(`INSERT INTO blood_donors (full_name,gender,birthday,country,state,city,pincode,blood_group,phone,address) values (${elements.full_name},${elements.gender},${elements.birthday},${elements.country},${elements.state},${elements.city},${elements.pincode},${elements.blood_group},${elements.phone},${elements.address})`, (err, row) => {
+                if (err) {
+                    res.render('dashboard/donors/create', {
+                        username: req.user.username,
+                        message: err,
+                        type: 'alert-danger'
+                    });
+                }
+                res.render('dashboard/donors/create', {
+                    username: req.user.username,
+                    message: `Successfully Registered Donor with Donor ID #${row.insertId}`,
+                    type: 'alert-success'
+                });
+            });
+        } catch (ex) {
+            res.render('dashboard/donors/create', {
+                username: req.user.username,
+                message: ex,
+                type: 'alert-danger'
+            });
+        }
+
     });
 };
 
